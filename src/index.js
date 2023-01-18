11e6b1872d91be1bbffc0f930fe5c3d52ef36436
Sani Rogers <sanirogers138@gmail.com>
2023-01-18 10:57:56 -0500

    done form submit practice

--- src/index.js
+++ src/index.js
@@ -29,10 +29,29 @@ window.addEventListener("load", (e) => {
     
     coolPointsForm.addEventListener("submit",(e) => {
         e.preventDefault();
-        const currentCoolPoints = document.querySelector("input[type='number']");
+        const currentCoolPoints = document.querySelector("input[name='cool-points-add']");
         const adder = parseInt(currentCoolPointsLabel.textContent);
         const numberSelection = parseInt(currentCoolPoints.value);
         currentCoolPointsLabel.textContent = adder + numberSelection;
+    })
+
+    const coolMultiplierForm = document.querySelector("#cool-multiplier-form");
+    coolMultiplierForm.addEventListener("submit",(e) => {
+        e.preventDefault();
+        const currentCoolPoints = document.querySelector("input[name='cool-points-multiplier']");
+        const multiplier = parseInt(currentCoolPointsLabel.textContent);
+        const numberSelection = parseInt(currentCoolPoints.value);
+        currentCoolPointsLabel.textContent = multiplier * numberSelection;
+    })
+
+    const pictureForm = document.querySelector("#picture-form")
+    pictureForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const imageUrlInput = document.querySelector("input[name='url']");
+        const newImage = document.createElement("img");
+        const picturesContainer = document.querySelector("#pictures-container");
+        newImage.src = imageUrlInput.value;
+        picturesContainer.append(newImage);
     })
 })
 
